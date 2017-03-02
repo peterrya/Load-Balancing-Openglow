5271e2a87bbf226f689cdb6ec05b04bdc22e8de7
peterrya <peterrya@onid.oregonstate.edu>
2017-03-02 11:25:10 -0800

    Updated loadbal.py
    put in mininet/custom run with cd mininet/custom
    sudo mn --custom loadbal.py -v output --topo mytopo --mac --arp --switch
    ovsk --controller remote
    
    on the command line run "upserv" to run a SimpleHTTPServer on each
    server and "cliwget [int]" to have each client wget from the
    loadbalancer (and therefor the servers)

--- loadbal.py
+++ loadbal.py
@@ -1,46 +1,63 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+Creates 1 switch with 1010 hosts, 10 servers and 1000 clients
 """
 
 from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.log import output, error
+
+servlist = []
+clilist = []
+
+def upserv( self, line ):
+    "upserv starts a HTTP server on every server host"
+    net = self.mn
+    #help(net) 
+    for x in range (0, 9):
+        net.__getitem__(servlist[x]).cmd('python -m SimpleHTTPServer 80 &')
+CLI.do_upserv = upserv
+
+def cliwget( self, num ):
+    "cliwget has each client wget 10.0.0.4"
+    net = self.mn
+    nu = int(num)
+    while (nu > 0):
+	nu = nu - 1
+    	for x in range (0,99):
+		print net.__getitem__(clilist[x]).cmd('wget 10.1.0.254')
+CLI.do_cliwget = cliwget
 
 class MyTopo( Topo ):
-    "Simple topology example."
+    "Load Balancing topology."
 
     def __init__( self ):
-        "Create custom topo."
+        "Create Load Balancing topo."
 
         # Initialize topology
         Topo.__init__( self )
 
 	# Add hosts
-	servlist = []
-	clilist = []
-	
-	for x in range(1,1000):
-		hostname = 'client'+str(x)
-		clilist.append(self.addHost(hostname))
 	
 	for x in range(1,10):
 		hostname = 'server'+str(x)
-		servlist.append(self.addHost(hostname))
-	
+		ipad = '10.1.0.' + str(x)
+		servlist.append(self.addHost(hostname, ip=ipad))
+
+	for x in range(1,100):
+		hostname = 'zclient'+str(x)
+		clilist.append(self.addHost(hostname))
+		
 	# Add switches
 	switchname = 's'+str(1) 
         rightSwitch = self.addSwitch( switchname )
 
         # Add links
-	for x in range(0,999):
-        	self.addLink( rightSwitch, clilist[x] )
-	
 	for x in range(0,9):
-		self.addLink( rightSwitch, servlist[x] )
-
+        	self.addLink( rightSwitch, servlist[x] )
+	
+	for x in range(0,99):
+		self.addLink( rightSwitch, clilist[x] )
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
+
